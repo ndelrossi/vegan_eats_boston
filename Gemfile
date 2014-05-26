29a41b346708addc596ec45797171bb50357f568
Nick DelRossi <nickwtf@gmail.com>
2014-05-26 10:42:41 -0400

    Finish placeholder layout

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ ruby '2.0.0'
 
 gem 'rails', '4.0.5'
 gem 'pg', '0.15.1'
+gem 'bootstrap-sass', '2.3.2.0'
+gem 'sprockets', '2.11.0'
 
 group :development, :test do
   gem 'rspec-rails', '2.13.1'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,47 +2,27 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'Vegan Eats Boston'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Vegan Eats Boston')
-    end
-
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Vegan Eats Boston")
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
+    it { should have_content('Vegan Eats Boston') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
   describe "Places page" do
+    before { visit places_path }
 
-    it "should have the content 'Places'" do
-      visit '/static_pages/places'
-      expect(page).to have_content('Places')
-    end
-
-    it "should have the title 'Places'" do
-      visit '/static_pages/places'
-      expect(page).to have_title("Vegan Eats Boston | Places")
-    end
+    it { should have_content('Places') }
+    it { should have_title(full_title('Places')) }
   end
 
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Vegan Eats Boston | About Us")
-    end
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
   end
 end
