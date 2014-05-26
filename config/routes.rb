3c2cbda0b8ac3b4e6642fe1a3f0032038f8fc40d
Nick DelRossi <nickwtf@gmail.com>
2014-05-26 15:51:46 -0400

    update header with logo

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ VeganEatsBoston::Application.routes.draw do
   resources :users
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
-  match '/places',    to: 'static_pages#places',    via: 'get'
+  match '/places',  to: 'static_pages#places',  via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   get "static_pages/home"
   get "static_pages/places"
