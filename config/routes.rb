23dd7be885e8abb62dd1c4bbfc3aeea43c067fa3
Nick DelRossi <nickwtf@gmail.com>
2014-05-28 08:18:30 -0400

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 VeganEatsBoston::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/places',  to: 'static_pages#places',  via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   get "static_pages/home"
