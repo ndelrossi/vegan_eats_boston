48b29ed2227b016d0ea09e408842d4b98908ef1f
Nick DelRossi <nickwtf@gmail.com>
2014-06-15 08:50:39 -0400

    add user posts to user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @posts = Post.where(:user => @user).paginate(page: params[:page], :per_page => 6)
   end
   
   def new
