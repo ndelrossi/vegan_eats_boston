9c42446bbb20cf8667fbd4926411b9082253241c
Nick DelRossi <nickwtf@gmail.com>
2014-06-15 08:16:33 -0400

    Fix typo on sign-up welcome message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Vegan Eats Boston!"
+      flash[:success] = "Welcome to Vegan Eats Boston!"
       redirect_to @user
     else
       render 'new'
