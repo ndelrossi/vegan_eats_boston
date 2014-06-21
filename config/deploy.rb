01266ba27e238dee7353f057d7133ccdce8de4e2
Nick DelRossi <nickwtf@gmail.com>
2014-06-21 16:49:20 -0400

    another try2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,41 +1,23 @@
-#require 'bundler/capistrano'
-
 set :application, 'veganeatsboston'
 set :repo_url, 'git@github.com:ndelrossi/vegan_eats_boston.git'
+
 set :deploy_to, '/home/deploy/vegan_eats_boston'
-set :scm, :git
-set :branch, "master"
-set :user, "deploy"
-set :use_sudo, false
-set :rails_env, "production"
-set :deploy_via, :copy
-set :ssh_options, { :forward_agent => true, :port => 4321 }
-set :keep_releases, 5
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
 
   desc "Symlink shared config files"
   task :symlink_config_files do
     run "#{ sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
   end
 
-  # NOTE: I don't use this anymore, but this is how I used to do it.
-  desc "Precompile assets after deploy"
-  task :precompile_assets do
-    run <<-CMD
-      cd #{ current_path } &&
-      #{ sudo } bundle exec rake assets:precompile RAILS_ENV=#{ rails_env }
-    CMD
-  end
-
-  desc "Restart applicaiton"
+  desc 'Restart application'
   task :restart do
-    run "#{ try_sudo } touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
   end
-end
 
-after "deploy", "deploy:symlink_config_files"
-after "deploy", "deploy:restart"
-after "deploy", "deploy:cleanup"
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+end
