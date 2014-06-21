c812722521dbdfadba0ae3b2e5b6a86b6dcb8b03
Nick DelRossi <nickwtf@gmail.com>
2014-06-21 16:42:53 -0400

    trying new deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,64 +1,43 @@
-# config valid only for Capistrano 3.1
-lock '3.2.1'
+require 'bundler/capistrano'
 
 set :application, 'veganeatsboston'
 set :repo_url, 'git@github.com:ndelrossi/vegan_eats_boston.git'
-
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-
-# Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/home/deploy/vegan_eats_boston'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+set :scm, :git
+set :branch, "master"
+set :user, "deploy"
+set :use_sudo, false
+set :rails_env, "production"
+set :deploy_via, :copy
+set :ssh_options, { :forward_agent => true, :port => 4321 }
+set :keep_releases, 5
+default_run_options[:pty] = true
+server "veganeatsboston.com", :app, :web, :db, :primary => true
 
 namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
 
   desc "Symlink shared config files"
   task :symlink_config_files do
     run "#{ sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
   end
-  
-  desc 'Restart application'
+
+  # NOTE: I don't use this anymore, but this is how I used to do it.
+  desc "Precompile assets after deploy"
+  task :precompile_assets do
+    run <<-CMD
+      cd #{ current_path } &&
+      #{ sudo } bundle exec rake assets:precompile RAILS_ENV=#{ rails_env }
+    CMD
+  end
+
+  desc "Restart applicaiton"
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
+    run "#{ try_sudo } touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
   end
-
-  after :publishing, 'deploy:restart'
-  after :finishing, 'deploy:cleanup'
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
 end
+
+after "deploy", "deploy:symlink_config_files"
+after "deploy", "deploy:restart"
+after "deploy", "deploy:cleanup"
