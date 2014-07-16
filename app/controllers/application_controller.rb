da382cd1bffa420b11d0ba832d754f407cefccaf
Nick DelRossi <nickwtf@gmail.com>
2014-07-16 08:59:06 -0400

    Fix cache issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :set_cache_buster
   protect_from_forgery with: :exception
   include SessionsHelper
-
-  before_filter :set_cache_buster
 
   def set_cache_buster
     response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
