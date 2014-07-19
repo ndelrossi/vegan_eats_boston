d777972f67dc71725294e406cfb0f1a831096b66
Nick DelRossi <nickwtf@gmail.com>
2014-07-19 08:30:12 -0400

    Update password_resets edit view. Fix RecordNotFound error

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,20 +5,25 @@ class PasswordResetsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     user.send_password_reset if user
-    flash[:notice] = "Email sent with password reset instructions."
+    flash[:info] = "Email sent with password reset instructions."
     redirect_to root_url
   end
 
   def edit
-    @user = User.find_by_password_reset_token!(params[:id])
+    unless @user = User.find_by_password_reset_token(params[:id])
+      flash[:danger] = "Reset link has expired. Please try again"
+      redirect_to new_password_reset_path
+    end
   end
 
   def update
     @user = User.find_by_password_reset_token!(params[:id])
     if @user.password_reset_sent_at < 2.hours.ago
-      redirect_to new_password_reset_path, :alert => "Password reset has expired."
+      flash[:danger] = "Password reset has expired. Please try again"
+      redirect_to new_password_reset_path
     elsif @user.update_attributes(params.permit![:user])
-      redirect_to root_url, :notice => "Password has been reset!"
+      flash[:success] = "Password has been reset!"
+      redirect_to root_url
     else
       render :edit
     end
