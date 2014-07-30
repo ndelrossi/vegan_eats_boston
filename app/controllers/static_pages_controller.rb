db1555c2b9472bfb64ba1f0e49c032cf1143690f
Nick DelRossi <nickwtf@gmail.com>
2014-07-30 06:46:51 -0400

    Remove limits on admin static page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -17,8 +17,8 @@ class StaticPagesController < ApplicationController
   def admin
     @posts = Post.page(params[:page]).per(10)
     @comments = Comment.page(params[:page]).per(10)
-    @users = User.limit(10).page(params[:page]).per(10)
-    @places = Place.limit(10).page(params[:page]).per(10)
-    @reviews = Review.limit(10).page(params[:page]).per(10)
+    @users = User.page(params[:page]).per(10)
+    @places = Place.page(params[:page]).per(10)
+    @reviews = Review.page(params[:page]).per(10)
   end
 end
