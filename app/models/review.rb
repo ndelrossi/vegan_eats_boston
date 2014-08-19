874647e5163a78ea912c93bc75ed17c2df82d79d
Nick DelRossi <nickwtf@gmail.com>
2014-08-19 06:59:09 -0400

    Change default scope for reviews to be created_at DESC

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,5 @@ class Review < ActiveRecord::Base
   belongs_to :place, counter_cache: true
   validates :user_id, presence: true
   validates :place_id, presence: true
+  default_scope -> { order('created_at DESC') }
 end
