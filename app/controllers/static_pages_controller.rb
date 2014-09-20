12bed240163177e6764f98aba444481a72d4ac1d
Nick DelRossi <nickwtf@gmail.com>
2014-09-20 08:55:09 -0400

    Add pagination to places on admin page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -18,7 +18,7 @@ class StaticPagesController < ApplicationController
   def admin
     @posts = Post.page(params[:page]).per(10)
     @users = User.page(params[:page]).per(10)
-    @places = Place.page(params[:page]).per(25)
+    @places = Place.page(params[:page]).page(params[:page]).per(20)
     @reviews = Review.page(params[:page]).per(10)
   end
 end
