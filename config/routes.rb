2f2460fdd6b681bdeedab95076c50ebd217175d0
Nick DelRossi <nickwtf@gmail.com>
2014-10-31 07:19:37 -0400

    Clean up routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 VeganEatsBoston::Application.routes.draw do
 
+  root  'static_pages#home'
+
   resources :users
   resources :posts do
     member do
@@ -11,7 +13,7 @@ VeganEatsBoston::Application.routes.draw do
   resources :reviews
   resources :sessions, only: [:new, :create, :destroy]
   resources :password_resets
-  root  'static_pages#home'
+
   match '/signup',            to: 'users#new',            via: 'get'
   match '/signin',            to: 'sessions#new',         via: 'get'
   match '/signout',           to: 'sessions#destroy',     via: 'delete'
@@ -19,66 +21,9 @@ VeganEatsBoston::Application.routes.draw do
   match '/about',             to: 'static_pages#about',   via: 'get'
   match '/admin',             to: 'static_pages#admin',   via: 'get'
   match '/posts_index_admin', to: 'posts#index_admin',    via: 'get'
-  #match '/posts/approve',     to: 'posts#approve',        via: 'put'
-  #match '/posts/unapprove',   to: 'posts#unapprove',      via: 'put'
   match '/users/activate/:id',to: 'users#activate',       via: 'get',     as: 'users_activate'
 
-  get "/404", :to => "errors#not_found"
-  get "/422", :to => "errors#unacceptable"
-  get "/500", :to => "errors#internal_error"
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  get "/404", to: "errors#not_found"
+  get "/422", to: "errors#unacceptable"
+  get "/500", to: "errors#internal_error"
 end
