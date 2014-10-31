ac8e391f9f0b6c73e02c6dcd3905d497037e53b5
Nick DelRossi <nickwtf@gmail.com>
2014-10-31 07:33:56 -0400

    Update routes file to use get/post/detete etc... instead of match

--- config/routes.rb
+++ config/routes.rb
@@ -14,16 +14,15 @@ VeganEatsBoston::Application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :password_resets
 
-  match '/signup',            to: 'users#new',            via: 'get'
-  match '/signin',            to: 'sessions#new',         via: 'get'
-  match '/signout',           to: 'sessions#destroy',     via: 'delete'
-  match '/blog',              to: 'static_pages#blog',    via: 'get'
-  match '/about',             to: 'static_pages#about',   via: 'get'
-  match '/admin',             to: 'static_pages#admin',   via: 'get'
-  match '/posts_index_admin', to: 'posts#index_admin',    via: 'get'
-  match '/users/activate/:id',to: 'users#activate',       via: 'get',     as: 'users_activate'
+  get 'signup'             => 'users#new'
+  get 'signin'             => 'sessions#new'
+  delete 'signout'         => 'sessions#destroy'
+  get 'blog'               => 'static_pages#blog'
+  get 'about'              => 'static_pages#about'
+  get 'admin'              => 'static_pages#admin'
+  get 'users/activate/:id' => 'users#activate',     as: 'users_activate'
 
-  get "/404", to: "errors#not_found"
-  get "/422", to: "errors#unacceptable"
-  get "/500", to: "errors#internal_error"
+  get "/404" => "errors#not_found"
+  get "/422" => "errors#unacceptable"
+  get "/500" => "errors#internal_error"
 end
