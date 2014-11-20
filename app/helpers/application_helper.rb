5081a902627b69eb097d45e4a3e73c4b86884089
Nick DelRossi <nickwtf@gmail.com>
2014-11-20 07:48:13 -0500

    Extract code for website logo to helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,10 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+  def website_logo
+    link_to image_tag("veb_logo.png", alt: "Vegan Eats Boston"), 
+      root_path, 
+      id: "logo"
+  end
 end
