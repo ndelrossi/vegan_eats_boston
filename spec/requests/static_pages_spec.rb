c130af5a1f029b1f5926dc950bdbe9200818bf80
Nick DelRossi <nickwtf@gmail.com>
2014-12-01 08:36:19 -0500

    Stop checking for page specific content that is on all pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,6 @@ describe "Static pages" do
     let!(:post) { create(:post, title: "Foo", content: "Bar", approved: true) }
     before { visit root_path }
 
-    it { is_expected.to have_content('Vegan Eats Boston') }
     it { is_expected.to have_title(full_title('')) }
     it { is_expected.to_not have_title('| Home') }
 
@@ -20,15 +19,13 @@ describe "Static pages" do
 
   describe "Places page" do
     before { visit places_path }
-
-    it { is_expected.to have_content('Places') }
+    it { is_expected.to have_content "results" }
     it { is_expected.to have_title(full_title('Places')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { is_expected.to have_content('About') }
     it { is_expected.to have_title(full_title('About Us')) }
   end
 end
