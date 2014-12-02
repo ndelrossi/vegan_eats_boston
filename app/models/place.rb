88dd1aacf41586356089d58360f4bd8683e05962
Nick DelRossi <nickwtf@gmail.com>
2014-12-02 06:42:13 -0500

    Fit lines to < 80 chars

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,12 +1,14 @@
 class Place < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
-  has_attached_file :primary_image, s3_permissions: :private, s3_server_side_encryption: :aes256,
-    :default_url => "/assets/missing-place.jpg",
-    :styles => {
-      :original => ["200x200>",:jpg] },
-    :convert_options => {
-      :original => "-quality 70 -strip -resize x400 -resize '400x<' 
-          -resize 50% -gravity center -crop 200x200+0+0 +repage" }
+  has_attached_file :primary_image, 
+                    s3_permissions: :private, 
+                    s3_server_side_encryption: :aes256,
+                    default_url: "/assets/missing-place.jpg",
+                    styles: { original: ["200x200>",:jpg] },
+                    convert_options: { original: "-quality 70 -strip 
+                                       -resize x400 -resize '400x<' 
+                                       -resize 50% -gravity center 
+                                       -crop 200x200+0+0 +repage" }
 
   validates_attachment_content_type :primary_image, :content_type => /\Aimage\/.*\Z/
   geocoded_by :full_address
