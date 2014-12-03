dcbd31ebb8d6ae4a65ba58af4a8d0b6ea7e62886
Nick DelRossi <nickwtf@gmail.com>
2014-12-03 08:47:53 -0500

    Add test for admin editing user

--- spec/features/admin_manages_users_spec.rb
+++ spec/features/admin_manages_users_spec.rb
@@ -1,26 +1,34 @@
 require 'rails_helper'
 
 feature "Admin manages users" do
-  scenario "with non-admin account" do
-    user = create(:user)
-
-    sign_in user
-    visit admin_dashboard_path
-
-    expect(page).to have_content('You are not authorized to view that page.')
-    expect(page).not_to have_content('Admin Dashboard')
-  end
-  
   scenario "deleting a user" do
     admin = create(:admin)
     user = create(:user, name: "UserFoo")
 
     sign_in admin
     visit_admin_users
-    expect(page).to have_content("UserFoo")
-
     find(:xpath, "//a[@href='/admin/users/#{user.id}']").click
+
+    expect(page).to have_content("User deleted.")
     visit_admin_users
+    expect(page).not_to have_content("UserFoo")
+  end
+
+  scenario "editing a user" do
+    admin = create(:admin)
+    user = create(:user, name: "UserFoo")
+
+    sign_in admin
+    visit_admin_users
+    find(:xpath, "//a[@href='/users/#{user.id}/edit']").click
+    fill_in "Name",                       with: "UserBar"
+    fill_in "user_password",              with: "foobar"
+    fill_in "user_password_confirmation", with: "foobar"
+    click_button "Save changes"
+
+    expect(page).to have_content("Profile updated")
+    visit_admin_users
+    expect(page).to have_content("UserBar")
     expect(page).not_to have_content("UserFoo")
   end
 
