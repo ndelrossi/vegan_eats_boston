c880ab2d45cc87a95aed5a66d1bda0fa94b76ca8
Nick DelRossi <nickwtf@gmail.com>
2014-12-03 07:24:05 -0500

    Add test to check if non-adim has a link to admin page

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -8,6 +8,7 @@ feature "User signs in" do
 
     expect(page).to have_title("Vegan Eats Boston | #{user.name}")
     expect(page).to have_content('Sign out')
+    expect(page).to_not have_content('Admin Page')
   end
 
   scenario "with invalid password" do
