ac1f51946190c03150b4afa5e0ec86ed992890ce
Nick DelRossi <nickwtf@gmail.com>
2014-12-10 08:03:41 -0500

    Add feature spec for visitor sorting by rating

--- spec/features/visitor_filters_places_spec.rb
+++ spec/features/visitor_filters_places_spec.rb
@@ -11,13 +11,22 @@ feature "Visitor filters places" do
                              address_city: "Somerville",
                              rating: 2) }
 
-  scenario "visitor filters by name", js: true do
+  before do
     visit places_path
+  end
 
+  scenario "visitor filters by name", js: true do
     fill_in "contains", with: "Foo"
 
     expect(page).to have_content("FooBiz")
     expect(page).to have_content("FooBaz")
     expect(page).not_to have_content("Bar")
+  end
+
+  scenario "visitor sorts places by rating", js: true do
+    click_button("Sort by")
+    choose('sort_rating')  
+
+    expect(page.text).to match(/Bar.*FooBiz.*FooBaz/)
   end
 end
